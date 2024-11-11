c24db472b38bdddebacf7d9972690af385449c62
VMatt <v.matt0826@gmail.com>
2024-11-11 12:11:51 +0100

    fix: UserEntity  IDs assigned automatically

--- backend/src/main/java/hu/unideb/inf/webshop/data/entity/UserEntity.java
+++ backend/src/main/java/hu/unideb/inf/webshop/data/entity/UserEntity.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Table(name = "users")
 public class UserEntity {
     @Id
-    //@GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column(name="first_name")
     private String firstName;
