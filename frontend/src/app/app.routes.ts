c595d1ec8a51ae93c9ae8b27a9d8e0dd80d3486d
VMatt <v.matt0826@gmail.com>
2024-11-21 18:23:14 +0100

    added page not found

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { UsersComponent } from './users/users.component';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
     { path: 'users', component: UsersComponent },
+    { path: '', component: HomeComponent },
+     { path: '**', component: PageNotFoundComponent },
+
 ];
