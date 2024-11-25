397066dc0b7859568281b5ac63ce39ab8150c2e4
Vajas Benjámin <vajas.ben1227@gmail.com>
2024-11-25 20:56:35 +0100

    add sample data for database to resource

--- backend/src/main/resources/sqlscripts/create_tables.sql
+++ backend/src/main/resources/sqlscripts/create_tables.sql
@@ -12,6 +12,7 @@ CREATE TABLE orders (
     user_id BIGINT,
     payment_status VARCHAR(50),
     status VARCHAR(50),
+    product_name VARCHAR(250),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
@@ -34,4 +35,29 @@ CREATE TABLE products (
     price INT
 );
 
+INSERT INTO users (first_name, last_name, email_address) VALUES
+('John', 'Doe', 'john.doe@example.com'),
+('Jane', 'Smith', 'jane.smith@example.com'),
+('Alice', 'Brown', 'alice.brown@example.com'),
+('Bob', 'White', 'bob.white@example.com');
 
+INSERT INTO products (size, weight, name, price) VALUES
+(10, 500, 'Laptop', 1000),
+(5, 300, 'Tablet', 500),
+(2, 100, 'Smartphone', 800),
+(1, 50, 'Smartwatch', 200),
+(20, 7000, 'Desktop', 1500);
+
+INSERT INTO orders (date, user_id, payment_status, status) VALUES
+(NOW(), 1, 'Paid', 'Shipped'),
+(NOW(), 2, 'Pending', 'Processing'),
+(NOW(), 3, 'Paid', 'Delivered'),
+(NOW(), 4, 'Failed', 'Cancelled');
+
+INSERT INTO products_orders (product_id, order_id, amount) VALUES
+(1, 1, 1),  -- 1 Laptop in Order 1
+(2, 1, 2),  -- 2 Tablets in Order 1
+(3, 2, 1),  -- 1 Smartphone in Order 2
+(4, 2, 1),  -- 1 Smartwatch in Order 2
+(5, 3, 1),  -- 1 Desktop in Order 3
+(1, 3, 1);  -- 1 Laptop in Order 3
